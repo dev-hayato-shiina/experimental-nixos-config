b5b3cf753969ff7aa2506b2112bbf40cfb64db1b
dev-hayato-shiina <dev.hayato.shiina@gmail.com>
2026-08-15 01:15:35 +0900

    add: ファイルの内容をコピーするシェルスクリプトの追加

---
+++ copy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env zsh
+clear
+
+TIMESTAMP=$(date +"%Y-%m-%d-%H-%M")
+OUTPUT_FILE="${TIMESTAMP}.txt"
+
+{
+  printf '===== tree =====\n\n'
+  tree -a -I ".git"
+  find . -type f \
+    ! -name "copy.sh" \
+    ! -name "*.txt" \
+    ! -path "./.git/*" \
+    ! -name ".gitignore" \
+    ! -name "README.md" \
+    ! -name "flake.lock" \
+    -print0 |
+  while IFS= read -r -d '' f; do
+    printf '\n===== %s =====\n\n' "$f"
+    cat "$f"
+  done
+} > "$OUTPUT_FILE"
+
+echo "Saved: $OUTPUT_FILE"
