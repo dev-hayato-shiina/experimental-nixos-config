46f9e90952020f7beeb830cbd27de75d4c52fc86
dev-hayato-shiina <dev.hayato.shiina@gmail.com>
2026-08-15 20:25:09 +0900

    fix: Quickshellの設定ファイルを配置するパスを変更

--- home/alpha/default.nix
+++ home/alpha/default.nix
@@ -9,7 +9,7 @@
 
   xdg.configFile."niri/config.kdl".source = ./config.kdl;
   xdg.configFile."alacritty/alacritty.toml".source = ./alacritty.toml;
-  xdg.configFile."quickshell/main/shell.qml".source = ./shell.qml;
+  xdg.configFile."quickshell/shell.qml".source = ./shell.qml;
 
   home.stateVersion = "26.05";
 
