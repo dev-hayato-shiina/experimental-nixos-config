e693a8c4a2caec35bf2800d1e10b3f458086cf2e
dev-hayato-shiina <dev.hayato.shiina@gmail.com>
2026-08-15 14:38:59 +0900

    add: ユーザーalphaにGitHubのSSHキー関連の設定を追加

--- home/alpha/default.nix
+++ home/alpha/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 
 {
   home.username = "alpha";
@@ -9,4 +9,40 @@
   xdg.configFile."quickshell/main/shell.qml".source = ./shell.qml;
 
   home.stateVersion = "26.05";
+
+  # TODO: 分割
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+
+    matchBlocks = {
+      "dev-hayato-shiina" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "${config.home.homeDirectory}/.ssh/dev-hayato-shiina-key";
+        extraOptions = {
+          TCPKeepAlive = "yes";
+          IdentitiesOnly = "yes";
+        };
+      };
+      "sheena-develop" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "${config.home.homeDirectory}/.ssh/sheena-develop-key";
+        extraOptions = {
+          TCPKeepAlive = "yes";
+          IdentitiesOnly = "yes";
+        };
+      };
+    };
+  };
+
+  home.file = {
+    ".ssh/dev-hayato-shiina-key.pub" = {
+      source = ./../../ssh/dev-hayato-shiina-key.pub;
+    };
+    ".ssh/sheena-develop-key.pub" = {
+      source = ./../../ssh/sheena-develop-key.pub;
+    };
+  };
 }
